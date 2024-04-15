78f0ae2165fceeee33b0045a5d5ee27f77fe6310
Alejandro Campano <a.campano.galan@avanade.com>
2024-04-15 18:21:07 +0200

    01 Done

--- CityInfo/CityInfo.API/Program.cs
+++ CityInfo/CityInfo.API/Program.cs
@@ -1,19 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); //Crea el host de la aplicacion
 
-// Add services to the container.
+// Add services to the container. SERVICES.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Registra los servicios de la aplicacion swagger necesarios
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Instancia del builder para nuestra aplicación
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. Construye la inyeccion de dependencias
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(); //Middleware controla las peticiones HTTP
+    app.UseSwaggerUI(); //MIDDLEWARES
 }
 
 app.UseHttpsRedirection();
@@ -23,3 +24,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
